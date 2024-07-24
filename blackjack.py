038827c0e089a7bf4ebdd72329fcf2c921c7af75
Arka Mandal <arkamandal764@gmail.com>
2024-07-25 00:15:12 +0530

    wrote some function

--- blackjack.py
+++ blackjack.py
@@ -1,1 +1,13 @@
-print("hi")
+import random
+def get_choice():
+    options = list(range(12))
+    player_choice = print(f"Enter your card of choice from {options}: ")
+    device_choice = random.choice(options)
+    return options
+    
+def card_draw(player):
+    print("Do you want to add a card or stay?")
+    list_choice = ["Pull","Stay"]
+    if player == "Pull":
+        get_choice()
+        
