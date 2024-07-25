2d67007073e1bf2698dd5276624f3e15a39e8b0f
Arka Mandal <arkamandal764@gmail.com>
2024-07-25 16:25:16 +0530

    wrote some function

--- blackjack.py
+++ blackjack.py
@@ -1,13 +1,49 @@
 import random
-def get_choice():
-    options = list(range(12))
-    player_choice = print(f"Enter your card of choice from {options}: ")
-    device_choice = random.choice(options)
-    return options
+options = list(range(12))
+def player_choice():
+    player = print(f"Enter your card of choice from {options}: ")
+    return player
+
+def device_choice():
+     device = random.choice(options)
+     return device
     
+def rounds():
+    no_rounds = print("Enter no. of rounds: ")
+    return no_rounds
+
 def card_draw(player):
-    print("Do you want to add a card or stay?")
-    list_choice = ["Pull","Stay"]
-    if player == "Pull":
-        get_choice()
-        
+    print("Do you want to pull a card or stay? (Pull, Stay): ")
+    no_rounds = rounds()
+    for i in range(1, no_rounds+1):
+        if player == "Pull":
+            player_choice()
+        else:
+            continue
+
+def sum_win(player_sum , device_sum):
+    no_rounds = rounds()
+    sum = random.choice(options)
+    player = player_choice()
+    device = device_choice()
+    for i in range(1, no_rounds+1):
+        player_sum = sum + player
+        device_sum = sum + device
+    if player_sum > 21 and device_sum<21:
+        print("You lost!")
+    elif device_sum > 21 and player_sum<21:
+        print("You won!")
+    elif player_sum > 21 and device_sum > 21 :
+        print("Disqualification!")
+    while(player_sum<21 and device_sum<21):
+        if player_sum == device_sum:
+            print("It's a Tie!")
+        elif player_sum < device_sum:
+            print("You lost!")
+        else:
+            print("You won!")
+
+player = player_choice()
+computer = device_choice()
+
+round_number = rounds()
